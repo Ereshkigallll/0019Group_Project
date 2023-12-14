4941ac03e148d3ef85cf0903a003237822d0381d
Huarui Yang <146243657+2333-hr@users.noreply.github.com>
2023-12-14 05:21:17 +0000

    Update LCD.cs

--- UNITY C#/LCD.cs
+++ UNITY C#/LCD.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro; // 引用TextMeshPro命名空间
+using TMPro; // Reference the TextMeshPro namespace
 
 public class LedDisplay : MonoBehaviour
 {
-    public TextMeshProUGUI cityText; // 用于显示城市名称的Text Mesh Pro UI组件
-    public TextMeshProUGUI aqiText; // 用于显示AQI数值的Text Mesh Pro UI组件
+    public TextMeshProUGUI cityText; // Text Mesh Pro UI component for displaying city names
+    public TextMeshProUGUI aqiText; // Text Mesh Pro UI component for displaying AQI values
 
-    // 设置并更新LED显示屏上的文本信息
+    // Set and update text information on the LED display
     public void SetDisplay(string city, int aqi)
     {
         cityText.text = city;
