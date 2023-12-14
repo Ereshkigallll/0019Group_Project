fba5a351968cf1cd4338cbe061fdefaba7cf3a83
Huarui Yang <146243657+2333-hr@users.noreply.github.com>
2023-12-14 05:20:18 +0000

    Update Instrument panel pointer rotation.cs

--- UNITY C#/Instrument panel pointer rotation.cs
+++ UNITY C#/Instrument panel pointer rotation.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class GaugePointer : MonoBehaviour
 {
-    public Transform pointerTransform; // 指针的Transform组件
+    public Transform pointerTransform; // Transform component of the pointer
 
-    // 假设每种污染物指标的最大值
+    // Assumed maximum values for each pollutant indicator
     private float maxValuePM25 = 300;
     private float maxValuePM10 = 300;
     private float maxValueNO2 = 300;
@@ -14,20 +14,20 @@ public class GaugePointer : MonoBehaviour
     public void SetPointerValue(float value, string type)
     {
         float angle = 0f;
-        // 根据类型和数值计算出旋转角度
+        // Calculate the rotation angle based on the type and value
         switch (type)
         {
             case "PM2.5":
-                angle = Mathf.Lerp(0, 42, value / maxValuePM25); 
+                angle = Mathf.Lerp(0, 42, value / maxValuePM25); // Interpolate between 0 and 42 degrees for PM2.5
                 break;
             case "PM10":
-                angle = Mathf.Lerp(42, 84, value / maxValuePM10); 
+                angle = Mathf.Lerp(42, 84, value / maxValuePM10); // Interpolate between 42 and 84 degrees for PM10
                 break;
             case "NO2":
-                angle = Mathf.Lerp(84, 126, value / maxValueNO2); 
+                angle = Mathf.Lerp(84, 126, value / maxValueNO2); // Interpolate between 84 and 126 degrees for NO2
                 break;
         }
-        // 应用旋转角度到指针
+        // Apply the rotation angle to the pointer
         pointerTransform.localEulerAngles = new Vector3(0, 0, -angle);
     }
 }
